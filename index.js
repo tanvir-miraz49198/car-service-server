c9b97de86f6cd6f54fb15a056627cfb8be071fa2
tanvir491981 <89649985+tanvir491@users.noreply.github.com>
2023-07-10 22:26:28 +0600

    delete server setup done

--- index.js
+++ index.js
@@ -80,6 +80,14 @@ app.get('/orders', async(req , res) => {
 
 
 
+    app.delete('/orders/:id', async(req , res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await confirmOrders.deleteOne(query);
+      res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
